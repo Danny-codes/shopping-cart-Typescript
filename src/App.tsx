6384b4a6ba281ef720037969a2b86e342537f36c
Daniel <danielreisp.2020@gmail.com>
2022-04-22 11:08:53 -0300

    Starting items Cart

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,10 @@ import { Drawer, LinearProgress, Grid, Badge } from "@material-ui/core";
 import { AddShoppingCart } from "@material-ui/icons";
 
 //styles
-import { Wrapper } from "./App.styles";
+import { StyledButton, Wrapper } from "./App.styles";
 import Item from "./ItemComponent/Item";
+import CartItem from "./CartItem/CartItem";
+import Cart from "./Cart/Cart";
 
 //types
 export type CartItemType = {
@@ -25,15 +27,20 @@ const getProducts = async (): Promise<CartItemType[]> => {
 };
 
 const App = () => {
+  const [cartOpen, setCartOpen] = useState(false)
+  const [cartItems, setCartItems] = useState([] as CartItemType[])
+
   const {data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts);
   console.log(data)
 
 
-  const getTotalItems = () => null
+  const getTotalItems = (items: CartItemType[]) => {
+    items.reduce((ack: number, item) =>  ack + item.amount, 0)
+  }
 
   const handleAddToCart = (clickedItem: CartItemType) => null
 
-  const hnadleRemoveFromCart = () => null
+  const handleRemoveFromCart = () => null
 
   if(isLoading){
     return <LinearProgress />;
@@ -44,6 +51,14 @@ const App = () => {
 
   return <div className="App">
     <Wrapper>
+      <Drawer anchor="right" open={cartOpen} onClose = {() => setCartOpen(false)}>
+        <Cart  cartItems={cartItems} addToCart={handleAddToCart} removeFromCart={handleRemoveFromCart}/>
+      </Drawer>
+      <StyledButton onClick = {() =>setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+          <AddShoppingCart />
+        </Badge>
+      </StyledButton>
       <Grid container spacing={3}>
         {data?.map(item => (
           <Grid item key={item.id} xs={12} sm= {4}>
